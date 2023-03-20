39bef41de8fae75786e15e86718bae65ebe5c044
Danny Qiu <danny.qiu@orange.com>
2023-03-20 14:05:29 +0000

    License

--- datacov/__init__.py
+++ datacov/__init__.py
@@ -1,3 +1,13 @@
+# Software Name: DataCov
+# Version: 0.1.0
+# SPDX-FileCopyrightText: Copyright (c) 2023 Orange
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This software is distributed under the BSD 3-Clause "New" or "Revised" License,
+# see the "LICENSE.txt" file for more details.
+#
+# Author: Danny Qiu <danny.qiu@orange.com>
+
 from .topo_generation import TopoGen
 from .coverage_shapes import Geom, VoroGeom, CircleGeom, EmpiricalGeom, DistGen, VoroSiteGeom
 from .analysis import precision_recall, precision_recall_site, evaluate, get_pr_band
--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
+# Software Name: DataCov
+# Version: 0.1.0
+# SPDX-FileCopyrightText: Copyright (c) 2023 Orange
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This software is distributed under the BSD 3-Clause "New" or "Revised" License,
+# see the "LICENSE.txt" file for more details.
+#
+# Author: Danny Qiu <danny.qiu@orange.com>
+
 from setuptools import setup, find_packages
 
 setup(
     name='DataCov',
-    version="0.0.1",
+    version="0.1.0",
     description="A small package for cell topography generation and coverage modelling",
     author='Danny Qiu',
     author_email='danny.qiu@orange.com',
-    license='To be defined.',
+    license='BSD 3-Clause',
     classifiers=[  
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Researchers",
